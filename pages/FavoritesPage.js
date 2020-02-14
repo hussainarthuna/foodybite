2789bef2ac1aa1080d685fb7f77aa522511c13f8
Hussain <hussainarthuna@gmail.com>
2020-02-14 15:10:04 +0300

    FavoritesPage.js and NotificationsPage.js Done

--- pages/FavoritesPage.js
+++ pages/FavoritesPage.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import CustomHeader from '../components/CustomHeader';
 import {Colors} from '../constants/Colors';
+import RestaurantCard from '../components/RestaurantCard';
+import {RestaurantData} from '../data/RestaurantData';
 
 const FavoritesPage = props => {
+
+    const _renderItem = ({item, index}) => {
+        return (
+            <RestaurantCard item={item}/>
+        );
+    };
+
     return (
         <View style={styles.screen}>
-            <CustomHeader title='Favorites' isHome/>
+            <CustomHeader title='My Favorites' isHome/>
+            <FlatList style={styles.list} data={RestaurantData} renderItem={_renderItem}/>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        backgroundColor:Colors.light
-    }
+    screen: {
+        flex: 1,
+        backgroundColor: Colors.light,
+    },
+    list: {
+        margin: 10,
+    },
 });
 
 export default FavoritesPage;
--- pages/NotificationsPage.js
+++ pages/NotificationsPage.js
@@ -1,21 +1,242 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import CustomHeader from '../components/CustomHeader';
+import {ListItem, List, Left, Thumbnail, Body, Right, Text} from 'native-base';
 import {Colors} from '../constants/Colors';
 
 const NotificationsPage = props => {
     return (
         <View style={styles.screen}>
             <CustomHeader title='Notifications' isHome/>
+            <ScrollView style={{flex: 1}}>
+                <List>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/1.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Brendan Hawkins</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>4:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/2.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Julie Hopper</Text>
+                            <Text note>Checked in at cheesecake factory</Text>
+                        </Body>
+                        <Right>
+                            <Text note>12:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/3.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Hussain Carter</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>1:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/4.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Fusion</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>1:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/5.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Steve Robs</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>5:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/6.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Tim Rock</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>5:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/1.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Brendan Hawkins</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>4:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/2.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Julie Hopper</Text>
+                            <Text note>Checked in at cheesecake factory</Text>
+                        </Body>
+                        <Right>
+                            <Text note>12:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/3.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Hussain Carter</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>1:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/4.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Fusion</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>1:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/5.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Steve Robs</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>5:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/6.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Tim Rock</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>5:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/1.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Brendan Hawkins</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>4:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/2.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Julie Hopper</Text>
+                            <Text note>Checked in at cheesecake factory</Text>
+                        </Body>
+                        <Right>
+                            <Text note>12:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/3.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Hussain Carter</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>1:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/4.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Fusion</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>1:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/5.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Steve Robs</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>5:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar noBorder>
+                        <Left>
+                            <Thumbnail source={require('../assets/images/6.jpg')}/>
+                        </Left>
+                        <Body>
+                            <Text>Tim Rock</Text>
+                            <Text note>Started following you</Text>
+                        </Body>
+                        <Right>
+                            <Text note>5:43 pm</Text>
+                        </Right>
+                    </ListItem>
+                </List>
+            </ScrollView>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        backgroundColor:Colors.light
-    }
+    screen: {
+        flex: 1,
+        backgroundColor: Colors.light,
+    },
 });
 
 export default NotificationsPage;
